47383fbc63e5643ad3fbdc71f4a2cdb6df3d4d0c
giresskenne <153892208+giresskenne@users.noreply.github.com>
2025-06-18 19:40:41 -0400

    Fix Vite build entry module resolution

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -31,6 +31,9 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
   }
 })

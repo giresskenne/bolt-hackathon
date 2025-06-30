d7964675d5c56485e5683bb588b8aec8d5144752
giresskenne <153892208+giresskenne@users.noreply.github.com>
2025-06-30 16:45:49 -0400

    Updated HomePage.jsx

--- webapp_fullstack/webapp/src/pages/HomePage.jsx
+++ webapp_fullstack/webapp/src/pages/HomePage.jsx
@@ -80,7 +80,7 @@ export default function HomePage() {
   Get the free Extension  
 </a>
                 <Link
-                  to="https://youtu.be/-4xBgZpEywU"
+                  href="https://youtu.be/-4xBgZpEywU"
                   className="border border-white/30 hover:border-primary px-8 py-4 rounded-xl text-lg font-semibold transition-all flex items-center justify-center"
                 >
                   <Play className="w-5 h-5 mr-2" />

b0226b96a561c336838433a9bb174c1f7f6ab2a2
giresskenne <153892208+giresskenne@users.noreply.github.com>
2025-06-30 16:53:07 -0400

    Updated TextArea.jsx

--- webapp_fullstack/webapp/src/components/TextArea.jsx
+++ webapp_fullstack/webapp/src/components/TextArea.jsx
@@ -59,7 +59,7 @@ export default function TextArea() {
               information in real-time.
             </p>
             {/* New div for extension download message */}
-            <div className="mt-4 p-3 bg-blue-600/10 border border-blue-500/20 rounded-lg text-blue-300 text-sm mx-auto max-w-md">
+            <div className="mt-4 p-3 bg-blue-600/10 border border-blue-600/20 rounded-lg text-blue-300 text-sm mx-auto max-w-md">
               <p>To try it out, Please download the Privly extension if you haven’t already</p>
               <a
                 href="https://chromewebstore.google.com/detail/prompt-scrubber/cpiimgglmignilhbjpkcdflkccjodahn?hl=en-GB&utm_source=ext_sidebar"
